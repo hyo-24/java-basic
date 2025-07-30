7fc46a77a2f10d1aca654b2cdcd9c14332b05c53
Elena Lee <tbfjs3976@icloud.com>
2025-07-30 10:23:56 +0900

    7/29

---
+++ src/static1/Data.java
@@ -0,0 +1,13 @@
+package static1;
+
+public class Data {
+    private int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
